7d7cff6015c997025b9d3a5fe9ea8bebf809187c
Vipin Madhavanunni <vipintm@gmail.com>
2015-12-24 22:38:06 +0100

    Updated-12-24-2015-22:37

---
+++ win32/stage2/AdHoc_bas.vb
@@ -0,0 +1,9 @@
+Option Strict Off
+Option Explicit On
+Module Module2
+	Public MaxNode As Short
+	Public Node() As Short
+	Public Hello() As Short
+	Public NodeID As Short
+	Public List As String
+End Module
